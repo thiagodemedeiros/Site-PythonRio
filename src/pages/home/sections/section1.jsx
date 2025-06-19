35582cdb09d5326e7f12df54413585b98dc3b8fb
Thiago Medeiros <thiagotitico1@gmail.com>
2025-06-19 01:22:42 -0300

    Alterando a posição dos botoes para que fique mais responsivo

--- src/pages/home/sections/section1.jsx
+++ src/pages/home/sections/section1.jsx
@@ -6,12 +6,16 @@ import Button from "../../../components/Button";
 export default function Section1 () {
     return (
         <div className="section">
-            <img src={Logo} alt="Logo PythonRio" className="logo1"/>
-            <div className="botoes">
-                <Button texto='Sobre' background='#FCFCFA' backgroundTexto='#48517C'/>
-                <Button texto='Palestrantes' background='#FCFCFA' backgroundTexto='#48517C'/>
-                <Button texto='Programação' background='#FCFCFA' backgroundTexto='#48517C'/>
+            <div className="logo-inscreva">
+                <img src={Logo} alt="Logo PythonRio" className="logo1"/>
                 <Button texto='INSCREVA-SE' background='#48517C' backgroundTexto='#FCFCFA' width='230px' height="50px" fontsize="24px"/>
+            </div>
+
+            <div className="botoes">
+                <Button texto='Sobre' background='#FCFCFA' backgroundTexto='#48517C' height="40px"/>
+                <Button texto='Palestrantes' background='#FCFCFA' backgroundTexto='#48517C' height="40px"/>
+                <Button texto='Programação' background='#FCFCFA' backgroundTexto='#48517C' height="40px"/>
+                
             </div>
         </div>
     )
