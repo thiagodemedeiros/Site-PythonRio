b8261476f8fa66efc71fa682a0e4a06c5015c7b3
Thiago Medeiros <thiagotitico1@gmail.com>
2025-06-19 01:08:13 -0300

    att primeira seção pronta para o desktop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-export default function Button ({ texto, background, backgroundTexto }) {
+export default function Button ({ texto,background,backgroundTexto,width='170px',height='auto',fontsize='20px' }) {
     return (
         <button 
             style={{
-                padding: '5px 50px',
+                padding: '5px 0px',
+                width: width,
+                height: height,
                 borderRadius: '30px',
                 backgroundColor: background,
                 color: backgroundTexto,
                 border: 'none',
                 fontWeight: 900,
-                fontSize: '24px'
+                fontSize: fontsize
             }}
         >
             {texto}
--- src/pages/home/sections/section1.jsx
+++ src/pages/home/sections/section1.jsx
@@ -7,7 +7,12 @@ export default function Section1 () {
     return (
         <div className="section">
             <img src={Logo} alt="Logo PythonRio" className="logo1"/>
-            <Button texto='Sobre' background='#FCFCFA' backgroundTexto='#48517C'/>
+            <div className="botoes">
+                <Button texto='Sobre' background='#FCFCFA' backgroundTexto='#48517C'/>
+                <Button texto='Palestrantes' background='#FCFCFA' backgroundTexto='#48517C'/>
+                <Button texto='Programação' background='#FCFCFA' backgroundTexto='#48517C'/>
+                <Button texto='INSCREVA-SE' background='#48517C' backgroundTexto='#FCFCFA' width='230px' height="50px" fontsize="24px"/>
+            </div>
         </div>
     )
 }
