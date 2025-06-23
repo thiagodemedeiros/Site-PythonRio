3268dbcdf680069be1c459984913384f551cdf7d
Thiago Medeiros <thiagotitico1@gmail.com>
2025-06-23 20:44:20 -0300

    Subindo a quarta seção

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Section1 from "./sections/section1";
 import Section2 from "./sections/Section2";
-import Section3 from "./sections/Section4";
+import Section3 from "./sections/Section3";
 import Section4 from "./sections/Section4";
 
 export default function Home () {
--- src/pages/home/sections/Section3.jsx
+++ src/pages/home/sections/Section3.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import './Section3.css'
+import CardPalestrante from '../../../components/CardPalestrante'
 
 export default function Section3 () {
     return (
-        <div>
-            
+        <div className="section-body">
+            <div class="titulo">
+                <h3>Conheça nossos palestrantes</h3>
+            </div>
+
+            <div className="secao1-cards">
+                <CardPalestrante/>
+                <CardPalestrante/>
+                <CardPalestrante/>
+                <CardPalestrante/>
+                <CardPalestrante/>
+            </div>
         </div>
     )
 }
