d4049d6aa48d4ca96dbc5a1381ac644a35d54d46
Thiago Medeiros <thiagotitico1@gmail.com>
2025-06-23 20:22:56 -0300

    Subindo a seção 3

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Section1 from "./sections/section1";
 import Section2 from "./sections/Section2";
+import Section3 from "./sections/Section3";
 
 export default function Home () {
     return (
         <div>
             <Section1/>
             <Section2/>
+            <Section3/>
         </div>
     )
 }
---
+++ src/pages/home/sections/Section3.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Section3.css'
+import Fundo from '../assets/fundo-section3.png'
+
+export default function Section3 () {
+    return (
+        <div className="Section3">
+            <h3>Vamos juntos escrever o próximo script dessa história?</h3>
+            <button>Increva-se</button>
+        </div>
+    )
+}
