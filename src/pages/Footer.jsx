b556a616be60ac4d846d6e7410f7d1dd54afb3a2
Thiago Medeiros <thiagotitico1@gmail.com>
2025-06-27 21:12:09 -0300

    Subindo o novo footer

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Footer.css'
+
+export default function footer () {
+    return (
+        <div className="footer">
+            <div className="botoes1">
+                <a href=""><h5>PythOnRio</h5></a>
+                <a href=""><h5>Palestrantes</h5></a>
+                <a href=""><h5>Sobre</h5></a>
+                <a href=""><h5>Inscreva-se</h5></a>
+            </div>
+
+            <div className="botoes2">
+                <p>
+                    Eu não sou designer <br/>
+                    Eu não sei o que to fazendo <br/>
+                    Se você chegou até aqui <br/>
+                    E gostou do que ta vendo <br/>
+                    Meu gato te agradece <br/>
+                    fim :D
+                </p>
+            </div>
+        </div>
+    )
+}
