8d4a6474a9dbbc46c68f762973c9810c7fefebf3
Thiago Medeiros <thiagotitico1@gmail.com>
2025-06-27 21:37:18 -0300

    Começando o header

--- src/pages/sobre/sobre.jsx
+++ src/pages/sobre/sobre.jsx
@@ -1,7 +1,11 @@
+import Header from "../Header"
 import Section1 from "./sections/section1"
 
 export default function Sobre () {
     return (
-        <Section1/>
+        <>
+            <Section1/>
+            <Header/>
+        </>
     )
 }
