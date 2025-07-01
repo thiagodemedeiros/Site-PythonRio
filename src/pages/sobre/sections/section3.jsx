bcec1b3de93b94030b6c71e5fb8b71284253b85f
Thiago Medeiros <thiagotitico1@gmail.com>
2025-06-30 21:53:52 -0300

    Subindo a pagina sobre

---
+++ src/pages/sobre/sections/section3.jsx
@@ -0,0 +1,9 @@
+import './section3.css'
+
+export default function Section3 () {
+    return (
+        <div className="sobre-section3">
+            <p>A ideia é juntar todo mundo em um espaço aberto, diverso e acolhedor pra trocar experiências, bater papo, aprender, ensinar e criar coisas incríveis juntos. Vai ter palestra, oficina, projeto aberto, networking (o famoso "vamos tomar um café?") e vários momentos especiais que só quem vive sabe.</p>
+        </div>
+    )
+}
--- src/pages/sobre/sobre.jsx
+++ src/pages/sobre/sobre.jsx
@@ -1,6 +1,8 @@
 import Header from "../Header"
 import Section1 from "./sections/section1"
 import Section2 from "./sections/section2"
+import Section3 from "./sections/section3"
+import Section4 from "./sections/section4"
 import Cta from "../Cta"
 import Footer from '../Footer'
 
@@ -10,6 +12,8 @@ export default function Sobre () {
             <Header/>
             <Section1/>
             <Section2/>
+            <Section3/>
+            <Section4/>
             <Cta/>
             <Footer/>
         </>
