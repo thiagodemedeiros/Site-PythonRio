ea1f49920b8c1afdb5984c3ae63a9e2a749573dd
Thiago Medeiros <thiagotitico1@gmail.com>
2025-07-07 22:25:54 -0300

    Subindo alterações para comunicação com a api, por momento somento local

--- src/components/InputFormulario.jsx
+++ src/components/InputFormulario.jsx
@@ -5,7 +5,13 @@ export default function InputFormulario (props) {
         <div>
             <div className='InputFormulario'>
                 <h4 className='InputFormulario-h4'>{props.textoh4}</h4>
-                <input className='InputFormulario-input' type="text" />
+                <input
+                    className='InputFormulario-input'
+                    type="text"
+                    name = {props.name}
+                    value={props.value}
+                    onChange={props.onChange}
+                />
             </div>
         </div>
     )
--- src/pages/inscreva/Inscreva.jsx
+++ src/pages/inscreva/Inscreva.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import InputFormulario from '../../components/InputFormulario'
 import HeaderInscreva from './HeaderInscreva'
 import Cta from '../Cta'
@@ -5,6 +6,48 @@ import Footer from '../Footer'
 import './Inscreva.css'
 
 export default function Inscreva () {
+
+    const [formData, setFormData] = useState ({
+        nome:'',
+        sobrenome:'',
+        email:'',
+        celular:'',
+        cpf:'',
+        data_de_nascimento:''
+    })
+
+    function handleChange(e) {
+        const {name,value} = e.target
+        setFormData(prev => ({ ...prev , [name]: value}))
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        try {
+            const response = await fetch('http://localhost:8000/usuarios', {
+                method:'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(formData)
+            })
+
+            const data = await response.json()
+            alert(`Usuário criado com ID: ${data.id}`)
+
+            setFormData ({
+                nome:'',
+                sobrenome:'',
+                email:'',
+                celular:'',
+                cpf:'',
+                data_de_nascimento:''
+            })
+
+            
+    } catch (error) {
+        alert('Erro ao enviar dados: ' + error.message)
+    }}
+
     return (
         <>
         <div className='inscreva-section'>
@@ -12,20 +55,21 @@ export default function Inscreva () {
 
             <h4 className='inscreva-section-h4'>Inscreva-se</h4>
 
-            <div className='inscreva-section-formulario'>
+            <form className='inscreva-section-formulario' onSubmit={handleSubmit}>
                 <div className='inscreva-section-formulario-2item'>
-                    <InputFormulario textoh4='Nome'/>
-                    <InputFormulario textoh4='Sobrenome'/>
+                    <InputFormulario textoh4='Nome' name='nome' value={formData.nome} onChange={handleChange}/>
+                    <InputFormulario textoh4='Sobrenome' name='sobrenome' value={formData.sobrenome} onChange={handleChange}/>
                 </div>
                 <div className='inscreva-section-formulario-2item'>
-                    <InputFormulario textoh4='E-mail'/>
-                    <InputFormulario textoh4='Celular'/>
+                    <InputFormulario textoh4='E-mail' name='email' value={formData.email} onChange={handleChange}/>
+                    <InputFormulario textoh4='Celular' name='celular' value={formData.celular} onChange={handleChange}/>
                 </div>
                 <div className='inscreva-section-formulario-2item'>
-                    <InputFormulario textoh4='CPF'/>
-                    <InputFormulario textoh4='Data de nascimento'/>
+                    <InputFormulario textoh4='CPF' name='cpf' value={formData.cpf} onChange={handleChange}/>
+                    <InputFormulario textoh4='Data de nascimento' name='data_de_nascimento' value={formData.data_de_nascimento} onChange={handleChange}/>
                 </div>
-            </div>
+                <button type='submit'>Enviar</button>
+            </form>
         </div>
 
         <Cta/>
