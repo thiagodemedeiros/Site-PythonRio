75a29d9a1402f328b84dfcc0e773f7afda7a88d3
Thiago Medeiros <thiagotitico1@gmail.com>
2025-07-21 20:53:07 -0300

    Subindo alteração para enviar proposta de palestra em todo canto

--- src/pages/Palestrantes.jsx
+++ src/pages/Palestrantes.jsx
@@ -1,18 +1,23 @@
 import CardPalestranteSobre from '../components/CardPalestranteSobre'
+import CtaPalestra from './home/sections/Section3'
 import './Palestrantes.css'
 
 export default function Palestrantes () {
     return (
-        <div className='section-palestrantes'>
-            <h3 className='section-palestrantes-h3'>Palestrantes</h3>
-
-            <div className='section-palestrantes-cards'>
-                <CardPalestranteSobre/>
-                <CardPalestranteSobre/>
-                <CardPalestranteSobre/>
-                <CardPalestranteSobre/>
-                <CardPalestranteSobre/>
-            </div>
+        <div>
+        <CtaPalestra/>
+        {/* <div className='section-palestrantes'> */}
+            {/* <CtaPalestra/> */}
+            {/* <h3 className='section-palestrantes-h3'>Palestrantes</h3> */}
+{/*  */}
+            {/* <div className='section-palestrantes-cards'> */}
+                {/* <CtaPalestra/> */}
+                {/* <CardPalestranteSobre/> */}
+                {/* <CardPalestranteSobre/> */}
+                {/* <CardPalestranteSobre/> */}
+                {/* <CardPalestranteSobre/> */}
+            {/* </div> */}
+        {/* </div> */}
         </div>
     )
 }
--- src/pages/programacao/programacao.jsx
+++ src/pages/programacao/programacao.jsx
@@ -3,14 +3,16 @@ import Secao2 from './secao2'
 import Secao3 from './secao3'
 import Secao4 from './secao4'
 import './programacao.css'
+import CtaPalestra from '../home/sections/Section3'
 
 export default function programacao() {
     return (
         <>
-            <Secao1/>
-            <Secao2/>
-            <Secao3/>
-            <Secao4/>
+            {/* <Secao1/> */}
+            {/* <Secao2/> */}
+            {/* <Secao3/> */}
+            {/* <Secao4/> */}
+            <CtaPalestra/>
         </>
     )
 }
