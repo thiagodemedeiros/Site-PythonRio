3b00a3b5cb8dd17d14942c4fccdf48695193b327
Thiago Medeiros <thiagotitico1@gmail.com>
2025-08-12 20:38:38 -0300

    Subindo o mapa

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,6 +3,7 @@ import Section1 from "./sections/section1";
 import Section2 from "./sections/Section2";
 import Section3 from "./sections/Section3";
 import Section4 from "./sections/Section4";
+import Section5 from "./sections/Section5";
 import Footer from "../Footer";
 
 export default function Home () {
@@ -12,6 +13,7 @@ export default function Home () {
             <Section2/>
             <Section3/>
             <Section4/>
+            <Section5/>
             <Footer/>
         </div>
     )
