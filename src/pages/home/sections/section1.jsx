1fb0ce6143e5fd80af75b0886561cdd4b583b9dc
Thiago Medeiros <thiagotitico1@gmail.com>
2025-10-18 23:16:56 -0300

    Subindo alterações

--- src/pages/home/sections/section1.jsx
+++ src/pages/home/sections/section1.jsx
@@ -9,14 +9,14 @@ export default function Section1 () {
             <div className="logo-inscreva">
                 <img src={Logo} alt="Logo PythonRio" className="logo1"/>
                 <div className="button1">
-                    {/* <a href="/inscreva"><Button texto='INSCREVA-SE' background='#48517C' backgroundTexto='#FCFCFA' width='230px' height="50px" fontsize="24px"/></a> */}
+                    <a href="/inscreva"><Button texto='INSCREVA-SE' background='#48517C' backgroundTexto='#FCFCFA' width='230px' height="50px" fontsize="24px"/></a>
                 </div>
             </div>
 
             <div className="botoes">
                 <a href="/sobre"><Button texto='Sobre' background='#FCFCFA' backgroundTexto='#48517C' height="40px"/></a>
-                {/* <a href="/palestrantes"><Button texto='Palestrantes' background='#FCFCFA' backgroundTexto='#48517C' height="40px"/></a> */}
-                {/* <a href="/programacao"><Button texto='Programação' background='#FCFCFA' backgroundTexto='#48517C' height="40px"/></a> */}
+                <a href="/palestrantes"><Button texto='Palestrantes' background='#FCFCFA' backgroundTexto='#48517C' height="40px"/></a>
+                <a href="/programacao"><Button texto='Programação' background='#FCFCFA' backgroundTexto='#48517C' height="40px"/></a>
             </div>
         </div>
     )
