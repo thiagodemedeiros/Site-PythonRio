de9a9fd4493edd498a703d052bc1596b307f3dbf
Thiago Medeiros <thiagotitico1@gmail.com>
2025-10-19 01:32:08 -0300

    header pronto para uso

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,25 @@
-export default function Button() {
+import React from "react";
+import styled from "styled-components";
+
+export default function Button({
+    texto, 
+    backgroundcolor = "FCFCFA",
+    width = "200px",
+    height = "50px",
+    color = "48517C",
+}) 
+{
+    const StyledButton = styled.button`
+        background-color: ${backgroundcolor};
+        width: ${width};
+        border-radius: 40px;
+        height: ${height};
+        font-weight: 700;
+        color: ${color};
+        cursor: pointer;
+    `
+
     return (
-        <button></button>
+        <StyledButton>{texto}</StyledButton>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
 import "../styles/Header.css"
 import logo from "../assets/Vector.png"
+import Button from "./Button"
 
 export default function Header() {
     return (
         <div className="header">
-            <img src={logo} alt="Logo da Python On Rio" />
+            <a href="/">
+                <img src={logo} alt="Logo da Python On Rio" />
+            </a>
 
             <div className="header-buttons">
-                <button>Próximo Evento</button>
-                <button>Sobre</button>
-                <button>INSCREVA-SE</button>
+                <Button texto={"Próximo evento"} width="150px"
+                height="30px"/>
+                <Button texto={"Sobre"} width="90px"
+                height="30px"/>
+                <Button texto="INSCREVA-SE" width="140px"
+                height="35px" backgroundcolor="#48517C" color="#FCFCFA"/>
             </div>
         </div>
     )
