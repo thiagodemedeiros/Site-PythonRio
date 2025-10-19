65a590d6ad9fcf01dcb8c31718fe36d715418291
Thiago Medeiros <thiagotitico1@gmail.com>
2025-10-19 17:24:33 -0300

    section 2 responsiva

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
         <>
             <Header/>
             <Section1/>
-            {/* <Section2/> */}
+            <Section2/>
             {/* <Section3/> */}
             {/* <Cta/> */}
             {/* <Footer/> */}
