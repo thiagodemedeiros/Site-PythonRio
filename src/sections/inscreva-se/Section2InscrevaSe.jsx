82fbe5a4cdbfe5d972f8bc8cb674256238f28c99
Thiago Medeiros <thiagotitico1@gmail.com>
2025-11-08 12:52:13 -0300

    Começando a adicionar palestrantes na pagina inscreva-se, cards já feitos e organizados, faltam ajustar detalhes

--- src/sections/inscreva-se/Section2InscrevaSe.jsx
+++ src/sections/inscreva-se/Section2InscrevaSe.jsx
@@ -8,20 +8,22 @@ export default function Section2InscrevaSe() {
             <div className='Section2InscrevaSe_Cards'>
                 <h4 className='Section2InscrevaSe_h4_Cards_1'>Para mais informações, clique no palestrante</h4>
 
-                <div className='Section2InscrevaSe_CardPalestrante'>
-                    <h4>Nome do palestrante</h4>
-                    <img src="https://img.freepik.com/fotos-gratis/gato-alegre-a-divertir-se_23-2151082419.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
-                    <h5>Titulo da palestra</h5>
-                </div>
-                <div className='Section2InscrevaSe_CardPalestrante'>
-                    <h4>Nome do palestrante</h4>
-                    <img src="https://img.freepik.com/fotos-gratis/gato-alegre-a-divertir-se_23-2151082419.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
-                    <h5>Titulo da palestra</h5>
-                </div>
-                <div className='Section2InscrevaSe_CardPalestrante'>
-                    <h4>Nome do palestrante</h4>
-                    <img src="https://img.freepik.com/fotos-gratis/gato-alegre-a-divertir-se_23-2151082419.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
-                    <h5>Titulo da palestra</h5>
+                <div className='Section2InscrevaSe_Cards_Wrap'>
+                    <div className='Section2InscrevaSe_CardPalestrante'>
+                        <h4>Nome do palestrante</h4>
+                        <img src="https://img.freepik.com/fotos-gratis/gato-alegre-a-divertir-se_23-2151082419.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
+                        <h5>Titulo da palestra</h5>
+                    </div>
+                    <div className='Section2InscrevaSe_CardPalestrante'>
+                        <h4>Nome do palestrante</h4>
+                        <img src="https://img.freepik.com/fotos-gratis/gato-alegre-a-divertir-se_23-2151082419.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
+                        <h5>Titulo da palestra</h5>
+                    </div>
+                    <div className='Section2InscrevaSe_CardPalestrante'>
+                        <h4>Nome do palestrante</h4>
+                        <img src="https://img.freepik.com/fotos-gratis/gato-alegre-a-divertir-se_23-2151082419.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
+                        <h5>Titulo da palestra</h5>
+                    </div>
                 </div>
             </div>
         </div>
