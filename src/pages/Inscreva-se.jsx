ddc2f55182a233b6237779247e37297eb2ec9fa8
Thiago Medeiros <thiagotitico1@gmail.com>
2025-11-08 12:50:03 -0300

    Começando a adicionar palestrantes na pagina inscreva-se, cards já feitos e organizados, faltam ajustar detalhes

--- src/pages/Inscreva-se.jsx
+++ src/pages/Inscreva-se.jsx
@@ -1,9 +1,11 @@
 import Section1InscrevaSe from '../sections/inscreva-se/Section1InscrevaSe'
+import Section2InscrevaSe from '../sections/inscreva-se/Section2InscrevaSe'
 
 export default function InscrevaSe() {
     return (
         <>
             <Section1InscrevaSe/>
+            <Section2InscrevaSe/>
         </>
     )
 }
