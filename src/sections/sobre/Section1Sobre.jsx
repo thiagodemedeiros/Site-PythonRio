415421563ef9709a6dedc65e4bb721bdea496b75
Thiago Medeiros <thiagotitico1@gmail.com>
2025-11-08 10:57:30 -0300

    Mudando o texto da pagina sobre

--- src/sections/sobre/Section1Sobre.jsx
+++ src/sections/sobre/Section1Sobre.jsx
@@ -9,8 +9,13 @@ export default function Section1() {
                         <h4>Sobre</h4>
                     </div>
                     <div className="Section1Sobre_texto_p">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                        <p>Depois de uma pausa de anos, a PythOnRio voltou mais forte do que nunca!</p>
+                        <p>Nosso primeiro evento teve mais inscrições do que o planeja, tanto para palestrantes quanto para participantes, e isso só nos deu mais força para continuar a crescer essa comunidade!</p>
+                        <p>Mas apesar se termos conquistado tudo isso na nossa volta, sabemos que existe mais a ser feito, queremos levar a tecnologia a todo canto do nosso RJ, tanto para o centro quanto para o interior.</p>
+                        <p>O foco agora é construirmos uma comunidade sólida, trazendo mais eventos, mais meetups, mais palestras e oportunidades de nos juntarmos.</p>
+                        <p>Será que vai ser possível trazer a PythOnRio de volta a como era antes? Não sei te dizer isso agora, mas sei que faremos o possível para torna-la maior do que é hoje.</p>
+                        <p>Seja você carioca, fluminense ou de qualquer parte do Brasil, se você quer fazer a diferença através da tecnologia, nós estaremos com você!</p>
+                        <p>Isso vai além do Python, isso vai além do Rio, isso vai além do Brasil</p>
                     </div>
                 </div>
             </div>
