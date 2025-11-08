d2bca26aeead1c8054b185bb8b74b7ecea82c355
Thiago Medeiros <thiagotitico1@gmail.com>
2025-11-08 11:37:00 -0300

    Subindo a section1 da pagina do inscreva-se

--- src/sections/inscreva-se/Section1InscrevaSe.jsx
+++ src/sections/inscreva-se/Section1InscrevaSe.jsx
@@ -2,12 +2,15 @@ import '../../styles/inscreva-se/Section1InscrevaSe.css'
 import LOGO_FIAP from '../../assets/InscrevaSe/Section1InscrevaSe/logofiap.png'
 
 export default function Section1InscrevaSe() {
-    return (
+    return ( <>
         <div className="Section1InscrevaSe">
             <div className='Section1InscrevaSe_patrocinio'>
                 <h4>Em parceria com:</h4>
                 <img src={LOGO_FIAP} alt="Logo da FIAP" />
             </div>
         </div>
-    )
+        <div className='Section1InscrevaSe_aba'>
+            <h4>Apresentamos o nosso próximo evento</h4>
+        </div>
+    </> )
 }
