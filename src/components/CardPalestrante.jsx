cffaa7fee55f72f0e5c53476f3a56c919172f9e5
Thiago Medeiros <thiagotitico1@gmail.com>
2025-11-11 19:12:00 -0300

    Refatorando para fazer o card dos palestrantes como um componente para facilitar o seu uso

---
+++ src/components/CardPalestrante.jsx
@@ -0,0 +1,19 @@
+import '../styles/components/CardPalestrante.css'
+
+const imgDefault = 'https://img.freepik.com/fotos-gratis/gato-alegre-a-divertir-se_23-2151082419.jpg?semt=ais_hybrid&w=740&q=80'
+
+export default function CardPalestrante({NomeDoPalestrante = 'Nome do Palestrante',
+                                        FotoDoPalestrante = imgDefault,
+                                        TituloDaPalestra = 'Titulo da Palestra',
+                                        LinkPaginaDoPalestrante = "/"
+}) {
+    return ( <>
+        <a href={LinkPaginaDoPalestrante}>
+            <div className='CardPalestrante'>
+                <h4>{NomeDoPalestrante}</h4>
+                <img src={FotoDoPalestrante} alt="Foto do Palestrante" />
+                <h5>{TituloDaPalestra}</h5>
+            </div>
+        </a>
+    </> )
+}
