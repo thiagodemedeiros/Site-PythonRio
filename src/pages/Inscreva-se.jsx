0591a77e8e50429ab4ded5d223f92c68a394c2e4
Thiago Medeiros <thiagotitico1@gmail.com>
2025-11-13 20:16:17 -0300

    Subindo mapa para guardar a localização

--- src/pages/Inscreva-se.jsx
+++ src/pages/Inscreva-se.jsx
@@ -1,11 +1,13 @@
 import Section1InscrevaSe from '../sections/inscreva-se/Section1InscrevaSe'
 import Section2InscrevaSe from '../sections/inscreva-se/Section2InscrevaSe'
+import Section3InscrevaSe from '../sections/inscreva-se/Section3InscrevaSe'
 
 export default function InscrevaSe() {
     return (
         <>
             <Section1InscrevaSe/>
             <Section2InscrevaSe/>
+            <Section3InscrevaSe/>
         </>
     )
 }
